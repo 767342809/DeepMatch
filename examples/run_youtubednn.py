9588a0901f15ceccbdd974a06420f73b86e0c07a
liangyue <liangyue@yit.com>
2020-11-03 11:27:02 +0800

    test

--- examples/run_youtubednn.py
+++ examples/run_youtubednn.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from deepctr.feature_column import SparseFeat, VarLenSparseFeat
-from .preprocess import gen_data_set, gen_model_input
+from deepctr.feature_column import SparseFeat, VarLenSparseFeat, DenseFeat
+from examples.preprocess import gen_data_set, gen_model_input
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.models import Model
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
     user_item_list = data.groupby("user_id")['movie_id'].apply(list)
 
-    train_set, test_set = gen_data_set(data, 0)
+    train_set, test_set = gen_data_set(data, 20)
 
     train_model_input, train_label = gen_model_input(train_set, user_profile, SEQ_LEN)
     test_model_input, test_label = gen_model_input(test_set, user_profile, SEQ_LEN)
@@ -42,14 +42,14 @@ if __name__ == "__main__":
     embedding_dim = 16
 
     user_feature_columns = [SparseFeat('user_id', feature_max_idx['user_id'], embedding_dim),
-                            SparseFeat("gender", feature_max_idx['gender'], embedding_dim),
+                            DenseFeat("gender"),
                             SparseFeat("age", feature_max_idx['age'], embedding_dim),
                             SparseFeat("occupation", feature_max_idx['occupation'], embedding_dim),
                             SparseFeat("zip", feature_max_idx['zip'], embedding_dim),
                             VarLenSparseFeat(SparseFeat('hist_movie_id', feature_max_idx['movie_id'], embedding_dim,
                                                         embedding_name="movie_id"), SEQ_LEN, 'mean', 'hist_len'),
                             ]
-
+    # feature_max_idx['gender'], embedding_dim
     item_feature_columns = [SparseFeat('movie_id', feature_max_idx['movie_id'], embedding_dim)]
 
     # 3.Define Model and train
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     user_embedding_model = Model(inputs=model.user_input, outputs=model.user_embedding)
     item_embedding_model = Model(inputs=model.item_input, outputs=model.item_embedding)
 
-    user_embs = user_embedding_model.predict(test_user_model_input, batch_size=2 ** 12)
+    user_embs = user_embedding_model.predict(test_model_input, batch_size=2 ** 12)
     # user_embs = user_embs[:, i, :]  # i in [0,k_max) if MIND
     item_embs = item_embedding_model.predict(all_item_model_input, batch_size=2 ** 12)
 
